6703bcce2213c90b3043159f53477ac16acd91fc
Thien Bui <thientanbui1@gmail.com>
2022-07-31 00:04:28 -0400

    Update SpacedLogger.java

--- SpacedLogger.java
+++ SpacedLogger.java
@@ -1,9 +1,24 @@
+package JavaWeek5;
 
-public class SpacedLogger {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
+	public class SpacedLogger implements Logger {
+		@Override
+		public void Log(String str) {
+			
+			StringBuilder log = new StringBuilder();
+			
+			for (int i = 0; i < str.length(); i++) {
+				log.append(str.charAt(i) + " ");
+			}
+			
+			System.out.println(log);
+			
+		}
+		
+		
+		@Override
+		public void Error(String error) {
+			SpacedLogger s = new SpacedLogger();
+			System.out.print("Error: ");
+			s.Log(error);
+		}
+	} 
